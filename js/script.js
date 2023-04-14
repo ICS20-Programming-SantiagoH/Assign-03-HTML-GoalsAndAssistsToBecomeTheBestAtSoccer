148ee48e1875ee2bd77f957b891661f9b9c3eff6
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-04-14 14:59:27 +0000

    Update

--- js/script.js
+++ js/script.js
@@ -10,54 +10,46 @@
  */
 function calculateGoals() {  
   
-  // initialize variables
-  let statmentGoals1 = ""
-  let statmentGoals2 = ""
-  
   // Data being collected 
   let goalsPerGame = parseFloat(document.getElementById('goalsPerGame').value);
-  let gamesPlayedGoals = parseFloat(document.getElementById('gamesPlayedGoals').value);
-  
+  let gamesPlayedGoals = parseInt(document.getElementById('gamesPlayedGoals').value);
+
   //Data being processed
-  let goals = goalsPerGame * gamesPlayed;
+  let goals = goalsPerGame * gamesPlayedGoals;
   let goalsToBeTheBestAtGoals = goals - 834;
 
   // If statments
   if (goalsToBeTheBestAtGoals > 834) {
-    statmentGoals1 = "Congratulations, you are already the best goal scorer!"
+    document.getElementById('goalsNeeded').innerHTML = "Congratulations, you are already the best goal scorer!"
   }
 
   if (goalsToBeTheBestAtGoals < 0) {
-    statmentGoals2 = "goalsToBeTheBestAtGoals * -1"
+    goalsToBeTheBestAtGoals = goalsToBeTheBestAtGoals * -1
+    // Data being shown
+  document.getElementById('goalsNeeded').innerHTML = 'You need ' + goalsToBeTheBestAtGoals.toFixed(2) + ' goals to become the greatest goal scorer of all time'
   }
   
-  // Data being shown
-  document.getElementById('goalsNeeded').innerHTML = 'You need ' + goalsToBeTheBestAtGoals.toFixed(2) + 'goals to become the greatest goal scorer of all time'
 }
 
 function calculateAssists() {  
   
-  // initialize variables
-  let statmentAssists1 = ""
-  let statmentAssists2 = ""
-  
   // Data being collected 
   let assistsPerGame = parseFloat(document.getElementById('assistsPerGame').value);
   let gamesPlayedAssists = parseFloat(document.getElementById('gamesPlayedAssists').value);
-  
+
   //Data being processed
   let assists = assistsPerGame * gamesPlayedAssists;
   let assistsToBeTheBestAtAssists = assists - 300;
-
+  
   // If statments
   if (assistsToBeTheBestAtAssists > 300) {
-    statment = "Congratulations, you are already the best assister!"
+    document.getElementById('assistsNeeded').innerHTML = "Congratulations, you are already the best assister!"
   }
 
   if (assistsToBeTheBestAtAssists < 0) {
-    statmentAssists2 = "assistsToBeTheBestAtAssists * -1"
-  }
-  
-  // Data being shown
-  document.getElementById('assistsNeeded').innerHTML = 'You need  ' + assistsToBeTheBestAtAssists.toFixed(2) + ' assists to become to greatest assister of all time.'
+    assistsToBeTheBestAtAssists = assistsToBeTheBestAtAssists * -1
+    // Data being shown
+  document.getElementById('assistsNeeded').innerHTML = 'You need ' + assistsToBeTheBestAtAssists.toFixed(2) + ' assists to become the greatest assister of all time'
+}
+
 }
